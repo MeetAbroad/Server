d0af9c87a45eb6979f0b78e335e68b9dae9144bd
Diogo Parrinha <piratanervo@gmail.com>
2016-05-11 15:00:32 +0200

    Finished #10 Search
    Added advanced search
    Fixed basic search bugs

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -157,20 +157,13 @@
 			
 			if($scope.adsearch.display == true)
 			{
+				$scope.search = angular.extend({}, $scope.search, $scope.adsearch);
+				
 				// Advanced search
-				$scope.searchResults = false;
-				$scope.searchLoading = false;
-				$scope.searchForm = true;
-			}
-			else
-			{
-				// Basic
-				$http.post('/search/basic', $scope.search, {
+				$http.post('/search/advanced', $scope.search, {
 					headers: {Authorization: 'Bearer '+auth.getToken()}
 				}).then(function successCallback(response){
 					data = response.data;
-					
-					console.log(data);
 					
 					if(data.length == 0)
 					{
@@ -179,6 +172,13 @@
 						$scope.searchForm = true;
 						
 						$scope.error = { message: "No results found." };
+						
+						jQuery(document).ready(function(){
+							// Scroll to top by default
+							jQuery('html, body').animate({
+							  scrollTop: 0
+							});
+						});
 					}
 					else
 					{
@@ -206,7 +206,63 @@
 						});
 					});
 					
+					$scope.searchResults = false;
 					$scope.searchLoading = false;
+					$scope.searchForm = true;
+				});
+			}
+			else
+			{
+				// Basic
+				$http.post('/search/basic', $scope.search, {
+					headers: {Authorization: 'Bearer '+auth.getToken()}
+				}).then(function successCallback(response){
+					data = response.data;
+					
+					if(data.length == 0)
+					{
+						$scope.searchResults = false;
+						$scope.searchLoading = false;
+						$scope.searchForm = true;
+						
+						$scope.error = { message: "No results found." };
+						
+						jQuery(document).ready(function(){
+							// Scroll to top by default
+							jQuery('html, body').animate({
+							  scrollTop: 0
+							});
+						});
+					}
+					else
+					{
+						$scope.results = data;
+						
+						$scope.searchLoading = false;
+						$scope.searchResults = true;
+						
+						jQuery(document).ready(function(){
+							// Scroll to top by default
+							jQuery('html, body').animate({
+							  scrollTop: 0
+							});
+						});
+					}
+					
+				}, function errorCallback(response){
+					data = response.data;
+					$scope.error = { message: data };
+
+					jQuery(document).ready(function(){
+						// Scroll to top by default
+						jQuery('html, body').animate({
+						  scrollTop: 0
+						});
+					});
+					
+					$scope.searchResults = false;
+					$scope.searchLoading = false;
+					$scope.searchForm = true;
 				});
 			}
 		};
--- routes/search.js
+++ routes/search.js
@@ -24,6 +24,11 @@ var getUser = function(req, res, next) {
     });
 }
 
+function isEmpty(obj) { 
+   for (var x in obj) { return false; }
+   return true;
+}
+
 // Get users by origincity
 router.post('/basic', auth, getUser, function(req, res, next) {
 	
@@ -65,6 +70,11 @@ router.post('/basic', auth, getUser, function(req, res, next) {
 					}
 				});
 			}
+			else if (search_clauses.firstname !== undefined || search_clauses.lastname !== undefined) // The user searched for a first and/or last name
+			{
+				// No matching against interests, just add the user to the results
+				users.push(doc);
+			}
 		});
 		
 		res.json(users);
@@ -79,6 +89,85 @@ router.post('/basic', auth, getUser, function(req, res, next) {
 */
 router.post('/advanced', auth, function(req, res, next) {
 	
+	var search_clauses = {};
+
+	if (req.body.origincountry !== undefined && req.body.origincountry !== null && req.body.origincountry !== "")
+		search_clauses.origincountry = req.body.origincountry;
+	
+	if (req.body.origincity !== undefined && req.body.origincity !== null && req.body.origincity !== "")
+		search_clauses.origincity = req.body.origincity;
+	
+	if (req.body.destinationcountry !== undefined && req.body.destinationcountry !== null && req.body.destinationcountry !== "")
+		search_clauses.destinationcountry = req.body.destinationcountry;
+	
+	if (req.body.destinationcity !== undefined && req.body.destinationcity !== null && req.body.destinationcity !== "")
+		search_clauses.destinationcity = req.body.destinationcity;
+	
+	if (req.body.firstname !== undefined && req.body.firstname !== null && req.body.firstname !== "")
+		search_clauses.firstname = req.body.firstname;
+	
+	if (req.body.lastname !== undefined && req.body.lastname !== null && req.body.lastname !== "")
+		search_clauses.lastname = req.body.lastname;
+	
+	// Only 'min' age set
+	if (req.body.minage !== undefined && req.body.minage !== null && req.body.minage !== ""
+		&& (req.body.maxage === undefined || req.body.maxage === null || req.body.maxage === ""))
+		search_clauses.age = { $gt: req.body.minage };
+	
+	// Only 'min' age set
+	if (req.body.maxage !== undefined && req.body.maxage !== null && req.body.maxage !== ""
+		&& (req.body.minage === undefined || req.body.minage === null || req.body.minage === ""))
+		search_clauses.age = { $lt: req.body.maxage };
+	
+	// Both min and max age set
+	if (req.body.minage !== undefined && req.body.minage !== null && req.body.minage !== ""
+		&& req.body.maxage !== undefined && req.body.maxage !== null && req.body.maxage !== "")
+		search_clauses.age =  { $gt: req.body.minage, $lt: req.body.maxage };
+		
+	console.log(search_clauses);
+	
+	if(isEmpty(search_clauses) && (req.body.selected === undefined || req.body.selected === null || req.body.selected.length === 0))
+		return res.status(400).json('You did not specify any criteria.');
+	
+    User.find(search_clauses, '-hash -salt -email -__v -fb -google').sort({'name': -1}).limit(10).populate('interests').exec(function (err, docs){
+        if (err) {
+			return next(err);
+		}
+		
+        if (!docs || typeof docs === 'undefined' || docs.length == 0) {
+			return next(new Error('No results found.'));
+		}
+		
+		var users = [];
+
+		// Time to go through each user
+		docs.forEach(function(doc){
+			
+			// Now it's time to check the interests
+			if(req.body.selected !== undefined && req.body.selected.length != 0 && req.body.selected !== null)
+			{
+				var added = false;
+				doc.interests.forEach(function(interest){
+					if(!added)
+					{
+						// If this interest is in our interests selection...
+						if(req.body.selected[interest.codename] !== undefined && req.body.selected[interest.codename] !== null && req.body.selected[interest.codename] === true)
+						{
+							users.push(doc);
+							added = true;
+						}
+					}
+				});
+			}
+			else
+			{
+				// No matching against 'Interests', just add the user to the results
+				users.push(doc);
+			}
+		});
+		
+		res.json(users);
+    });
 
 });
 
